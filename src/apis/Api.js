8135d4454adde1f56b6f3ec686aebc7a6e79a3e1
Madhu Chaudhary <madhu@MacBook-Air-3.local>
2025-01-16 22:59:06 +0545

    https implementation

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -25,29 +25,40 @@ const getConfig = () => ({
 });
 
 // Authentication APIs
-export const registerUserApi = (data) => Api.post("/api/user/create", data);
+export const registerUserApi = (data) =>
+  Api.post("https://localhost:5500/api/user/create", data);
 export const loginUserApi = (data) => Api.post("/api/user/login", data);
 export const forgotPasswordApi = (data) =>
-  Api.post("/api/user/forgot_password", data);
-export const verifyOtpApi = (data) => Api.post("/api/user/verify_otp", data);
+  Api.post("https://localhost:5500/api/user/forgot_password", data);
+export const verifyOtpApi = (data) =>
+  Api.post("https://localhost:5500/api/user/verify_otp", data);
 
 // User APIs
 export const getUserData = (userId) =>
-  Api.get(`/api/user/profile/${userId}`, getConfig());
-export const getAllUserData = () => Api.get("/api/user/all_user", getConfig());
-export const getUserDataById = (userId) => Api.get(`/api/user/user/${userId}`);
+  Api.get(`https://localhost:5500/api/user/profile/${userId}`, getConfig());
+export const getAllUserData = () =>
+  Api.get("https://localhost:5500/api/user/all_user", getConfig());
+export const getUserDataById = (userId) =>
+  Api.get(`https://localhost:5500/api/user/user/${userId}`);
 export const updateUserData = (userId, user) =>
-  Api.put(`/api/user/update/${userId}`, user, getConfig());
+  Api.put(
+    `https://localhost:5500/api/user/update/${userId}`,
+    user,
+    getConfig()
+  );
 export const delUserById = (userId) =>
-  Api.delete(`/api/user/delete_account/${userId}`, getConfig());
+  Api.delete(
+    `https://localhost:5500/api/user/delete_account/${userId}`,
+    getConfig()
+  );
 
 // Category APIs
 export const createCategoryApi = (data) =>
-  Api.post("/api/category/create", data, getConfig());
+  Api.post("https://localhost:5500/api/category/create", data, getConfig());
 export const getAllCategory = async () => {
   try {
     const response = await Api.get(
-      "api/category/get_all_category",
+      "https://localhost:5500/api/category/get_all_category",
       getConfig()
     );
     return response.data;
@@ -57,68 +68,105 @@ export const getAllCategory = async () => {
   }
 };
 export const getcaterogyById = (id) =>
-  Api.get(`/api/category/getcaterogybyid/${id}`);
+  Api.get(`https://localhost:5500/api/category/getcaterogybyid/${id}`);
 export const deleteCategory = (id) =>
-  Api.delete(`/api/category/delete_category/${id}`, getConfig());
+  Api.delete(
+    `https://localhost:5500/api/category/delete_category/${id}`,
+    getConfig()
+  );
 
 // Product APIs
 export const createProductApi = (data) =>
-  Api.post("/api/product/create", data, getConfig());
+  Api.post("https://localhost:5500/api/product/create", data, getConfig());
 export const getAllProducts = () =>
-  Api.get("/api/product/get_all_products", getConfig());
+  Api.get("https://localhost:5500/api/product/get_all_products", getConfig());
 export const getProductsByCategory = (categoryId) =>
-  axios.get(`/api/product/get_all_products?category=${categoryId}`);
+  axios.get(
+    `https://localhost:5500/api/product/get_all_products?category=${categoryId}`
+  );
 export const getSingleProduct = (id) =>
-  Api.get(`/api/product/get_single_product/${id}`, getConfig());
+  Api.get(
+    `https://localhost:5500/api/product/get_single_product/${id}`,
+    getConfig()
+  );
 export const updateProduct = (id, data) =>
-  Api.put(`/api/product/update_product/${id}`, data, getConfig());
+  Api.put(
+    `https://localhost:5500/api/product/update_product/${id}`,
+    data,
+    getConfig()
+  );
 export const deleteProduct = (id) =>
-  Api.delete(`/api/product/delete_product/${id}`, getConfig());
+  Api.delete(
+    `https://localhost:5500/api/product/delete_product/${id}`,
+    getConfig()
+  );
 
 // Cart APIs
 export const addToCartApi = (data) =>
-  Api.post("/api/cart/addToCart", data, getConfig());
+  Api.post("https://localhost:5500/api/cart/addToCart", data, getConfig());
 export const getCartByUserIDApi = (id) =>
-  Api.get(`/api/cart/getCartByUserID/${id}`, getConfig());
+  Api.get(`https://localhost:5500/api/cart/getCartByUserID/${id}`, getConfig());
 export const updateCartApi = (id, formData) =>
-  Api.put(`/api/cart/updateCart/${id}`, formData, getConfig());
+  Api.put(
+    `https://localhost:5500/api/cart/updateCart/${id}`,
+    formData,
+    getConfig()
+  );
 export const removeFromCartApi = (id) =>
-  Api.delete(`/api/cart/removeFromCart/${id}`, getConfig());
+  Api.delete(
+    `https://localhost:5500/api/cart/removeFromCart/${id}`,
+    getConfig()
+  );
 export const updateCartStatusApi = (data) =>
-  Api.put(`/api/cart/status`, data, getConfig());
+  Api.put(`https://localhost:5500/api/cart/status`, data, getConfig());
 
 // Order APIs
-export const getUserOrdersApi = () => Api2.get("/api/order/user", getConfig());
-export const getallOrdersApi = () => Api2.get("/api/order/get", getConfig());
+export const getUserOrdersApi = () =>
+  Api2.get("https://localhost:5500/api/order/user", getConfig());
+export const getallOrdersApi = () =>
+  Api2.get("https://localhost:5500/api/order/get", getConfig());
 export const createOrderApi = (data) =>
-  Api2.post("/api/order/create", data, getConfig());
+  Api2.post("https://localhost:5500/api/order/create", data, getConfig());
 export const updateOrderApi = (id, data) =>
-  Api.put(`/api/order/update/${id}`, data, getConfig());
+  Api.put(`https://localhost:5500/api/order/update/${id}`, data, getConfig());
 export const updateOrderStatusApi = (id, data) =>
-  Api.put(`/api/order/update/${id}`, data, getConfig());
+  Api.put(`https://localhost:5500/api/order/update/${id}`, data, getConfig());
 
 // Address APIs
 export const createAddress = (data) =>
-  Api.post("/api/address/shipping-address", data, getConfig());
+  Api.post(
+    "https://localhost:5500/api/address/shipping-address",
+    data,
+    getConfig()
+  );
 export const editAddress = (addressId, data) =>
   Api.put(
-    `/api/address/update-shipping-address/${addressId}`,
+    `https://localhost:5500/api/address/update-shipping-address/${addressId}`,
     data,
     getConfig()
   );
 export const getAddress = (id) =>
-  Api.get(`/api/address/getaddress/${id}`, getConfig());
+  Api.get(`https://localhost:5500/api/address/getaddress/${id}`, getConfig());
 export const getShippingAddressById = (addressId) =>
-  Api.get(`/api/address/getaddressbyaddressId/${addressId}`, getConfig());
+  Api.get(
+    `https://localhost:5500/api/address/getaddressbyaddressId/${addressId}`,
+    getConfig()
+  );
 export const deleteAddress = (addressId) =>
-  Api.delete(`/api/address/deleteaddress/${addressId}`, getConfig());
+  Api.delete(
+    `https://localhost:5500/api/address/deleteaddress/${addressId}`,
+    getConfig()
+  );
 
 // Review and Rating APIs
 export const createReviewApi = (data) =>
-  Api.post("/api/review/addReview", data, getConfig());
+  Api.post("https://localhost:5500/api/review/addReview", data, getConfig());
 export const updateReviewApi = (id) =>
-  Api.get(`/api/review/updateReview/${id}`);
+  Api.get(`https://localhost:5500/api/review/updateReview/${id}`);
 export const getReviewsByUserIDApi = (id) =>
-  Api.get(`/api/review/getReviewsByUserID/${id}`, getConfig());
+  Api.get(
+    `https://localhost:5500/api/review/getReviewsByUserID/${id}`,
+    getConfig()
+  );
 export const getReviewsByProductID = (id) =>
-  Api.get(`/api/review/getReviewsByProductID/${id}`);
+  Api.get(`https://localhost:5500/api/review/getReviewsByProductID/${id}`);
